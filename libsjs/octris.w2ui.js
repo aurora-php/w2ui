aa4e7989230fcee8f98875296d8792239b59400b
Harald Lapp <harald@octris.org>
2017-08-26 20:22:52 +0200

    added dependencies

---
+++ libsjs/octris.w2ui.js
@@ -0,0 +1,21 @@
+/*
+ * This file is part of the 'octris/w2ui' package.
+ *
+ * (c) Harald Lapp
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+/**
+ * Main javascript library for octris-w2ui.
+ *
+ * @copyright   copyright (c) 2017 by Harald Lapp
+ * @author      Harald Lapp <harald@octris.org>
+ */
+/**/
+
+
+if (!('octris' in window)) window['octris'] = {};
+
+if (!('w2ui' in octris)) octris.w2ui = {};
